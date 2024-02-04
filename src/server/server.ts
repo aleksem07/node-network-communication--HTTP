3fcf25e4d88d032acfdf493cd70d5df521802fca
aleksem07 <aleksem07@gmail.com>
2024-02-05 01:58:08 +0300

    feat: add nodemon

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,13 @@ import http from 'http';
 const port = 3000;
 
 export const server = http.createServer((req, res) => {
-  res.end('Hello Server');
+  console.log('Url:', req.url);
+  console.log('Тип запроса:', req.method);
+  console.log('User-Agent:', req.headers['user-agent']);
+  console.log('Все заголовки');
+  console.log(req.headers);
+
+  res.end('Hello Server12qw3');
 });
 
 server.listen(port, () => {
