db68fb3a6fedb316d8cad02710f4c1ad3249dd2a
aleksem07 <aleksem07@gmail.com>
2024-02-05 00:58:23 +0300

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2023: true,
-    node: true
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    'no-console': 1,
+  },
 };
