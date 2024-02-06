e2118bc5299a9aa5657f33f694592ad23e4badce
aleksem07 <aleksem07@gmail.com>
2024-02-07 00:15:46 +0300

    feat: add get

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # node-network-communication--HTTP
 
 `npm init`
-`"start:dev"`: "nodemon ./src/index.ts 3000",
-`"start:prod"`: "node ./dist/bundle.js",
+
+`"start"`: запускает в режиме dev
+`"start:prod"`: запускает в режиме prod
+
+`.env` - переменные среды
+
+порт 3005, по умолчанию 3000
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,16 +1,58 @@
 import http from 'http';
-const port = 3002;
+import { users } from '../data/users';
+import { homePage } from '../components/home-page';
+import { AppRoutes } from '../common/routes';
+import { validate } from 'uuid';
+
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 export const server = http.createServer((req, res) => {
-  console.log('Url:', req.url);
-  console.log('Тип запроса:', req.method);
-  console.log('User-Agent:', req.headers['user-agent']);
-  console.log('Все заголовки');
-  console.log(req.headers);
+  const { method, url } = req;
 
-  res.end('Hello Server');
+  if (!url) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+    return;
+  }
+
+  const userId = url.split('/').filter((item) => item)[2];
+
+  if (method === 'GET' && url === AppRoutes.USERS) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  } else if (method === 'GET' && url.startsWith(`${AppRoutes.USERS}/`)) {
+    console.log(url);
+    console.log('userId:' + userId);
+
+    if (!validate(userId)) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+      return;
+    }
+
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('User not found');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(user));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+    return;
+  }
+
+  if (method === 'GET' && url === AppRoutes.HOME) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(homePage);
+  }
 });
 
-server.listen(port, () => {
-  console.log(`The server is running on port:${port}`);
+server.listen(PORT, () => {
+  console.log(`The server is running on port:${PORT}`);
 });
