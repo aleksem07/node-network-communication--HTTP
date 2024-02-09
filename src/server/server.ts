114d79cd9fa436aa7831976ed7062b41bf6db5de
aleksem07 <aleksem07@gmail.com>
2024-02-10 01:17:38 +0300

    feat: add cluster

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,136 +1,108 @@
 import http from 'http';
-import { users } from '../data/users-init';
-import { homePage } from '../components/home-page';
+import { users } from '../data/users';
 import { AppRoutes } from '../common/routes';
 import { validate } from 'uuid';
-import { IUsers } from '../data/users-init';
+import { IUsers } from '../data/users';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
+import cluster from 'cluster';
+import os from 'os';
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT || 3000);
+const numCPUs = os.cpus().length;
 
-export const server = http.createServer((req, res) => {
-  const { method, url } = req;
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running. Listening on port ${PORT}`);
 
-  if (!url) {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
-    return;
+  for (let i = 0; i < numCPUs - 1; i++) {
+    cluster.fork();
   }
 
-  const userId = url.split('/').filter((item) => item)[2];
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code ${code} and signal ${signal}`);
+  });
+} else {
+  const workerPort: number = PORT + (cluster.worker?.id || 0);
+  const server = http.createServer((req, res) => {
+    const { method, url } = req;
 
-  if (method === 'GET' && url === AppRoutes.HOME) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(homePage);
-  }
-
-  if (method === 'GET' && url === AppRoutes.USERS) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(users));
-  } else if (method === 'GET' && url.startsWith(`${AppRoutes.USERS}/`)) {
-    if (!validate(userId)) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('userId is invalid (not uuid)');
-      return;
-    }
-
-    const user = users.find((user) => user.id === userId);
-    if (!user) {
+    if (!url) {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end("User  doesn't exist");
+      res.end('Not found');
       return;
     }
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(user));
-  } else if (method === 'POST' && url === AppRoutes.USERS) {
-    const body: Buffer[] = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = JSON.parse(Buffer.concat(body).toString());
-      const newUser: IUsers = {
-        id: uuidv4(),
-        ...parsedBody,
-      };
+    const userId = url.split('/').filter((item) => item)[2];
 
-      if (!newUser.username || !newUser.age || !newUser.hobbies) {
+    if (method === 'GET' && url === AppRoutes.USERS) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(users));
+    } else if (method === 'GET' && url.startsWith(`${AppRoutes.USERS}/`)) {
+      if (!validate(userId)) {
         res.writeHead(400, { 'Content-Type': 'text/plain' });
-        res.end('request body does not contain required fields (username or age or hobbies)');
+        res.end('userId is invalid (not uuid)');
         return;
       }
 
-      users.push(newUser);
-
-      fs.writeFile(
-        './src/data/users.js',
-        `exports.users = ${JSON.stringify(users, null, 2)};`,
-        (err) => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end(err.message);
-          }
-          res.writeHead(201, { 'Content-Type': 'application/json' });
-          res.end(`User created`);
-        }
-      );
-    });
-  } else if (method === 'DELETE' && url.startsWith(`${AppRoutes.USERS}/`)) {
-    if (!validate(userId)) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('userId is invalid (not uuid)');
-      return;
-    }
-
-    const userIndex = users.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end("User doesn't exist");
-      return;
-    }
-
-    users.splice(userIndex, 1);
-
-    fs.writeFile(
-      './src/data/users.js',
-      `exports.users = ${JSON.stringify(users, null, 2)};`,
-      (err) => {
-        if (err) {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end(err.message);
-        }
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(`User deleted`);
+      const user = users.find((user) => user.id === userId);
+      if (!user) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("User  doesn't exist");
+        return;
       }
-    );
-  } else if (method === 'PUT' && url.startsWith(`${AppRoutes.USERS}/`)) {
-    if (!validate(userId)) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('userId is invalid (not uuid)');
-      return;
-    }
 
-    const userIndex = users.findIndex((user) => user.id === userId);
-    if (userIndex === -1) {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end("User doesn't exist");
-      return;
-    }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(user));
+    } else if (method === 'POST' && url === AppRoutes.USERS) {
+      const body: Buffer[] = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const parsedBody = JSON.parse(Buffer.concat(body).toString());
+        const newUser: IUsers = {
+          id: uuidv4(),
+          ...parsedBody,
+        };
 
-    const body: Buffer[] = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = JSON.parse(Buffer.concat(body).toString());
-      users[userIndex] = {
-        ...users[userIndex],
-        ...parsedBody,
-      };
+        if (!newUser.username || !newUser.age || !newUser.hobbies) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('request body does not contain required fields (username or age or hobbies)');
+          return;
+        }
+
+        users.push(newUser);
+
+        fs.writeFile(
+          './src/data/users.js',
+          `exports.users = ${JSON.stringify(users, null, 2)};`,
+          (err) => {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end(err.message);
+            }
+            res.writeHead(201, { 'Content-Type': 'application/json' });
+            res.end(`User created`);
+          }
+        );
+      });
+    } else if (method === 'DELETE' && url.startsWith(`${AppRoutes.USERS}/`)) {
+      if (!validate(userId)) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('userId is invalid (not uuid)');
+        return;
+      }
+
+      const userIndex = users.findIndex((user) => user.id === userId);
+      if (userIndex === -1) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("User doesn't exist");
+        return;
+      }
+
+      users.splice(userIndex, 1);
 
       fs.writeFile(
         './src/data/users.js',
@@ -141,16 +113,54 @@ export const server = http.createServer((req, res) => {
             res.end(err.message);
           }
           res.writeHead(200, { 'Content-Type': 'application/json' });
-          res.end(`User updated`);
+          res.end(`User deleted`);
         }
       );
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not found');
-  }
-});
+    } else if (method === 'PUT' && url.startsWith(`${AppRoutes.USERS}/`)) {
+      if (!validate(userId)) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('userId is invalid (not uuid)');
+        return;
+      }
 
-server.listen(PORT, () => {
-  console.log(`The server is running on port:${PORT}`);
-});
+      const userIndex = users.findIndex((user) => user.id === userId);
+      if (userIndex === -1) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end("User doesn't exist");
+        return;
+      }
+
+      const body: Buffer[] = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const parsedBody = JSON.parse(Buffer.concat(body).toString());
+        users[userIndex] = {
+          ...users[userIndex],
+          ...parsedBody,
+        };
+
+        fs.writeFile(
+          './src/data/users.js',
+          `exports.users = ${JSON.stringify(users, null, 2)};`,
+          (err) => {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end(err.message);
+            }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(`User updated`);
+          }
+        );
+      });
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not found');
+    }
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Worker ${process.pid} started. Listening on port ${workerPort}`);
+  });
+}
